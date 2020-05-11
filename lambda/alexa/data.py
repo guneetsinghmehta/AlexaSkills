a2dc9bdeb6b7513c48b461d525897b322394f831
Guneet Mehta <mehtaguneet@mehtaguneet-macbookpro.roam.corp.google.com>
2020-05-10 17:41:00 -0700

    Working AddFriendIntent

---
+++ lambda/alexa/data.py
@@ -0,0 +1,10 @@
+from gettext import gettext as _
+
+WELCOME_MSG = _("Default. Hello! Welcome to Friend Book. A fun way to store friend birthdays.")
+WELCOME_REPROMPT_MSG = _("Default. This is Friend Book.")
+HELP_MSG = _("You can tell me your date of birth and I'll take note. You can also just say, 'register my birthday' and I will guide you. Which one would you like to try?")
+GOODBYE_MSG = _("Goodbye!")
+ERROR_MSG = _(
+    "Sorry, I couldn't understand what you said. Can you reformulate?")
+ERROR_TIMEZONE_MSG = _(
+    "I can't determine your timezone. Please check your device settings and make sure a timezone was selected. After that please reopen the skill and try again!")
---
+++ lambda/lambda_function.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+
+# This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK for Python.
+# Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+# session persistence, api calls, and more.
+# This sample is built using the handler classes approach in skill builder.
+import logging
+import ask_sdk_core.utils as ask_utils
+import os
+import json
+import locale
+import requests
+import calendar
+import gettext
+from datetime import datetime
+from pytz import timezone
+from ask_sdk_s3.adapter import S3Adapter
+s3_adapter = S3Adapter(bucket_name=os.environ["S3_PERSISTENCE_BUCKET"])
+
+from alexa import data
+
+from ask_sdk_core.skill_builder import CustomSkillBuilder
+from ask_sdk_core.dispatch_components import (
+    AbstractRequestHandler, AbstractRequestInterceptor, AbstractExceptionHandler)
+from ask_sdk_core.handler_input import HandlerInput
+
+from ask_sdk_model import Response
+
+from ask_sdk_core.utils import is_request_type, is_intent_name
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    """
+    Handler for Skill Launch
+    """
+
+    def can_handle(self, handler_input):
+        return is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        data = handler_input.attributes_manager.request_attributes["_"]
+        speech = data["WELCOME_MSG"]
+        reprompt = data["WELCOME_REPROMPT_MSG"]
+
+        handler_input.response_builder.speak(speech).ask(reprompt)
+        return handler_input.response_builder.response
+
+class AddFriendIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return ask_utils.is_intent_name("AddFriendIntent")(handler_input)
+        
+    def handle(self, handler_input):
+        return handler_input.response_builder.speak("In Friend intent handler").response
+        
+
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        data = handler_input.attributes_manager.request_attributes["_"]
+        speak_output = data["HELP_MSG"]
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        data = handler_input.attributes_manager.request_attributes["_"]
+        speak_output = data["GOODBYE_MSG"]
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .response
+        )
+
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Any cleanup logic goes here.
+
+        return handler_input.response_builder.response
+
+
+class IntentReflectorHandler(AbstractRequestHandler):
+    """The intent reflector is used for interaction model testing and debugging.
+    It will simply repeat the intent the user said. You can create custom handlers
+    for your intents by defining them above, then also adding them to the request
+    handler chain below.
+    """
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("IntentRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        data = handler_input.attributes_manager.request_attributes["_"]
+        intent_name = ask_utils.get_intent_name(handler_input)
+        speak_output = data["REFLECTOR_MSG"].format(intent_name)
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                .response
+        )
+
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    """Generic error handling to capture any syntax or routing errors. If you receive an error
+    stating the request handler chain is not found, you have not implemented a handler for
+    the intent being invoked or included it in the skill builder below.
+    """
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.error(exception, exc_info=True)
+        data = handler_input.attributes_manager.request_attributes["_"]
+        speak_output = data["ERROR_MSG"]
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+
+# The SkillBuilder object acts as the entry point for your skill, routing all request and response
+# payloads to the handlers above. Make sure any new handlers or interceptors you've
+# defined are included below. The order matters - they're processed top to bottom.
+
+
+class LocalizationInterceptor(AbstractRequestInterceptor):
+    """
+    Add function to request attributes, that can load locale specific data.
+    """
+
+    def process(self, handler_input):
+        skill_locale = handler_input.request_envelope.request.locale
+
+        # localized strings stored in language_strings.json
+        with open("language_strings.json") as language_prompts:
+            language_data = json.load(language_prompts)
+        # set default translation data to broader translation
+        data = language_data[skill_locale[:2]]
+        # if a more specialized translation exists, then select it instead
+        # example: "fr-CA" will pick "fr" translations first, but if "fr-CA" translation exists,
+        #          then pick that instead
+        if skill_locale in language_data:
+            data.update(language_data[skill_locale])
+        handler_input.attributes_manager.request_attributes["_"] = data
+
+        # configure the runtime to treat time according to the skill locale
+        skill_locale = skill_locale.replace('-','_')
+        locale.setlocale(locale.LC_TIME, skill_locale)
+        
+
+sb = CustomSkillBuilder(persistence_adapter=s3_adapter)
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(AddFriendIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn’t override your custom intent handlers
+
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+sb.add_global_request_interceptor(LocalizationInterceptor())
+
+
+lambda_handler = sb.lambda_handler()
